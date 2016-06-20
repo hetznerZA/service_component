691b5f4184487ddee3bd702e9d189150e7d9c28c
daneb <dane.balia@hetzner.co.za>
2016-06-20 06:36:38 +0200

    Linked up steps and orchestration

---
+++ features/step_definitions/cors.rb
@@ -0,0 +1,71 @@
+Given(/^a valid CORS configuration$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I am bootstrapping$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^successfully apply the CORS configuration$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I indicate 'success'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^an invalid CORS configuration$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I fail to bootstrap$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I indicate 'fail'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I notify 'invalid CORS configuration'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^fail to apply the CORS configuration$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I notify 'failure in bootstrapping CORS configuration'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^an unexpected occurrs$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I notify 'unexpected error in bootstrapping CORS confguration'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^specific ruleset$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^a request requires cross\-origin resource sharing$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^it matches the specific ruleset$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I grant access to the resource$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^it does not match the specific ruleset$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I deny access to the resource$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
