a9e3c7a13d76c3fef4ccd3783a966907035f29e0
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-06-28 12:50:39 +0200

    reverted earlier timeout changes

--- run_bdd_tests_in_docker.sh
+++ run_bdd_tests_in_docker.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
+export SOAR_DIR=/soar_testing/soar_sc
+export SERVICE_COMPONENT_DIR=/soar_testing/service_component
+export AUDIT_TEST_SERVICE_DIR=/soar_testing/soar_audit_test_service
 
-export SOAR_DIR=$(pwd)/soar_sc
-export SERVICE_COMPONENT_DIR=$(pwd)/service_component
-
-'rvm install ruby-2.3.0'
-'rvm install jruby-9.0.4.0'
-
-echo "Installing ruby and gems of SOAR_SC"
+echo "Injecting Audit test service jewel"
+cd $AUDIT_TEST_SERVICE_DIR
+export TEST_SERVICE=soar_audit_test_service
+cd $AUDIT_TEST_SERVICE_DIR/tfa/jewels/$TEST_SERVICE
+zip -rq ../$TEST_SERVICE.zip *
+cd $AUDIT_TEST_SERVICE_DIR
+cp tfa/jewels/*.zip $SOAR_DIR/jewels
 cd $SOAR_DIR
-rvm use .
-bundle install
+jewels/inject_jewel.sh $TEST_SERVICE
 
-echo "Installing ruby and gems of service_component"
-cd $SERVICE_COMPONENT_DIR
-rvm use .
-bundle install
+# echo "Installing ruby and gems of SOAR_SC"
+# cd $SOAR_DIR
+# rvm use jruby-9.0.4.0
+# gem install bundle
+# bundle install
+#
+#
+# echo "Installing ruby and gems of service_component"
+# cd $SERVICE_COMPONENT_DIR
+# rvm use ruby-2.3.0
+# gem install bundle
+# bundle install
 
 
 echo "Starting keep_running of soar_sc"
 cd $SOAR_DIR
-./keep_running.sh &
+rvm use jruby-9.0.4.0
+cd ..
+cd $SOAR_DIR
+export SOAR_TECH=rackup
+export RACK_ENV=development
+./soar_tech.sh
+rvm use jruby-9.0.4.0
+gem install bundle
+bundle install
+./keep_running.sh > /dev/null 2>&1 &
 
 echo "Running service component BDD tests"
 cd $SERVICE_COMPONENT_DIR
-bundle exec TEST_ORCHESTRATION_PROVIDER=tfa bundle exec cucumber features/bootstrap_with_audit* features/bootstrap_with_service_identifier.feature features/auditing_*
-
-
+rvm use ruby-2.3.0
+cd ..
+cd $SERVICE_COMPONENT_DIR
+rvm use ruby-2.3.0
+gem install bundle
+bundle install
+export TEST_ORCHESTRATION_PROVIDER=tfa
+bundle exec cucumber features/bootstrap_with_audit* features/bootstrap_with_service_identifier.feature features/auditing_*
