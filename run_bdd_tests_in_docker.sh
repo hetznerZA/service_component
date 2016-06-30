008af069b273621156ebf65d777fa53e9f1de34b
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-06-30 12:17:06 +0200

    Cleanup of test runner

--- run_bdd_tests_in_docker.sh
+++ run_bdd_tests_in_docker.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export SOAR_DIR=/soar_testing/soar_sc
-export SERVICE_COMPONENT_DIR=/soar_testing/service_component
+#This script must be run from the parent folder of the soar_sc, service_component repo folders
+export BASE_DIR=$(pwd)
+
+export SOAR_DIR=$BASE_DIR/soar_sc
+export SERVICE_COMPONENT_DIR=$BASE_DIR/service_component
+
+echo "Cleaning all repos involved in testing in preparation for jewel injection"
+cd $BASE_DIR/soar_sc && git checkout . && git clean -fdx
+#cd $BASE_DIR/service_component && git checkout . && git clean -fdx
+#cd $BASE_DIR/soar_audit_test_service && git checkout . && git clean -fdx
+
+echo "Import soar_audit_test_service to audit related testing"
+cd $BASE_DIR/soar_audit_test_service && ./import.sh
 
 echo "Starting keep_running of soar_sc"
 cd $SOAR_DIR
@@ -11,8 +22,18 @@ export RACK_ENV=development
 ./soar_tech.sh
 rvm use . && gem install bundle && bundle
 ./keep_running.sh > /dev/null 2>&1 &
+export KEEP_RUNNING_PID=$!
 
 echo "Running service component BDD tests"
 cd $SERVICE_COMPONENT_DIR
 rvm use . && gem install bundle && bundle
 TEST_ORCHESTRATION_PROVIDER=tfa bundle exec cucumber features/bootstrap_with_audit* features/bootstrap_with_service_identifier.feature features/auditing_*
+TEST_EXIT_CODE=$?
+
+echo "Stopping keep_running script and soar_sc service instance"
+kill $KEEP_RUNNING_PID
+$SOAR_DIR/stop.sh
+
+echo "Cleanly exit"
+cd $BASE_DIR
+exit $TEST_EXIT_CODE
