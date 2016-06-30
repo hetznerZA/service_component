d9bfeaeef69722415f863528b7d54aa6b83b07d0
Barney de Villiers <barney.de.villiers@hetzner.co.za>
2016-06-30 12:32:25 +0200

    attempt to fix rvm issues

--- run_bdd_tests_in_docker.sh
+++ run_bdd_tests_in_docker.sh
@@ -20,13 +20,14 @@ cp config/environment.yml.example config/environment.yml
 export SOAR_TECH=rackup
 export RACK_ENV=development
 ./soar_tech.sh
-rvm use . && gem install bundle && bundle
+rvm . && bundle
+
 ./keep_running.sh > /dev/null 2>&1 &
 export KEEP_RUNNING_PID=$!
 
 echo "Running service component BDD tests"
 cd $SERVICE_COMPONENT_DIR
-rvm use . && gem install bundle && bundle
+rvm use . && bundle
 TEST_ORCHESTRATION_PROVIDER=tfa bundle exec cucumber features/bootstrap_with_audit* features/bootstrap_with_service_identifier.feature features/auditing_*
 TEST_EXIT_CODE=$?
 
